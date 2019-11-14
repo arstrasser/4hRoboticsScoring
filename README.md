767d26aa0f30671912ba85bfa844ad773a37d92f
Alex Strasser (Laptop) <arstrasser@gmail.com>
2019-11-14 14:26:19 -0500

    Added documentation
    New comments for server.js
    Added server port config
    Created README.md

---
+++ README.md
@@ -0,0 +1,53 @@
+# 4-H Robotics Scoring System
+
+Developed by Alex Strasser 2019
+
+### Overview
+
+This scoring system was developed to help run 4-H robotics challenges at the Somerset County 4-H fair each year. It allows for live webcam/score display on a projector and referee scoring on mobile phones.
+
+This system consists of 3 components:
+- A webserver
+- A mobile application
+- A webcam display on the server
+
+The webserver was coded using Express to run on a Node.js server. It uses the [opencv4nodejs](https://github.com/justadudewhohacks/opencv4nodejs) to display current webcam views in a window on the server. The server also hosts a compiled angular app that can be accessed by phones on the same network as the server.
+
+The webserver and webcam code is stored in server.js
+
+The mobile web app code is stored in scoring-interface2019 in an [Angular](https://angular.io/) project. Every time this code is updated, run `npm build` and reload the server to update everything.
+
+### Setup
+
+First, clone/download this repository.
+
+One of the significant dependencies of the webserver is [`opencv4nodejs`](https://github.com/justadudewhohacks/opencv4nodejs). This library allows us to display the webcam stream in a local window and additionally do image processing to display the scores at the end of each round.
+
+This library has a dependency that must be installed globally **if you are on Windows**. If you are not on Windows, you do not need to install any additional packages.
+
+Windows: `npm i -g windows-build-tools`
+
+Personally, I have had lots of trouble getting it installed, but that is the hardest part. If you need additional help, see [here](https://github.com/felixrieseberg/windows-build-tools#readme)
+
+Now, you should be set to install everything else.
+- `npm i` - Install necessary components for the server. (This will take a long time and processing power: ~20 minutes)
+- `cd scoring-interface2019` - Go into the Angular project directory.
+- `npm i` - Install dependencies for the Angular project.
+- `ng build --prod` - Compiles the Angular code into a static app. This command should create a directory called scoring-build in the root directory of the repository.
+- `cd ..` - Go back into the root project directory.
+
+### Configuration
+
+To select a different camera device, open `server.js` and change the `const devicePort` to have a different value.
+
+If the device port is invalid, the server won't start.
+
+To select a different webserver port, open `server.js` and change the `const serverPort` to have a different value.
+
+### Running the Server
+
+To start the server: `node server.js`
+
+This should open a window for the webcam stream as well as a port for the webserver. You should be able to connect on the port logged into the server console from either a computer or phone **ON THE SAME NETWORK**. Just open chrome/safari on the phone and navigate to http://IP_ADDRESS:PORT
+
+This means your server computer must be on the same wifi network as the referee phones, or you will have to set up port forwarding.
--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const logger = require('morgan');
 const cv = require('opencv4nodejs');
 const path = require('path');
 
+// CONFIGURATION:
 const devicePort = 1;
-const camera = new cv.VideoCapture(devicePort);
+const serverPort = 80;
 
+//Variables
+const camera = new cv.VideoCapture(devicePort);
 const redVideo = new cv.VideoCapture('./media/red.mp4');
 const blueVideo = new cv.VideoCapture('./media/blue.mp4');
 const tieVideo = new cv.VideoCapture('./media/tie.mp4');
@@ -14,21 +17,27 @@ const tieVideo = new cv.VideoCapture('./media/tie.mp4');
 let displayMode = 0;
 
 let app = express();
-//app.use(favicon(path.join(__dirname, 'media', favicon.ico')));
+//Logs all requests for easy debugging
 app.use(logger('dev'));
 
+//Score storage
 let scores = {'red':0, 'blue':0};
 
+//Host the angular project index on the server
 app.use("/score/", express.static(path.join(__dirname, 'scoring-build')))
 
+//Host the angular project files on the server
 app.get('/score/*', (req, res) => {
   res.sendFile(path.join(__dirname, "scoring-build", "index.html"));
 })
 
+//Gets the current scores
 app.get('/currentScores', (req, res) => {
   res.status(200).json(scores);
 })
 
+//Request to update the score
+//Example: /updateScores?color=red&score=30
 app.get('/updateScores', (req, res) => {
   let newScore = parseInt(req.query.score);
   let team = req.query.color;
@@ -39,13 +48,16 @@ app.get('/updateScores', (req, res) => {
   res.status(200).json({"status":"Success"});
 })
 
+//Request to display the scores on the screen
 app.get('/publish', (req, res) => {
+  //Just updates the display mode for the later opencv code
   displayMode = 1;
   res.status(200).json({"status":"Success"});
 })
 
-app.listen(80, () => {
-  console.log("Listening on port 80...")
+//Start the server
+app.listen(serverPort, () => {
+  console.log("Webserver listening on port "+serverPort+"...")
 })
 
 let numDigits = (number) => {
@@ -53,14 +65,19 @@ let numDigits = (number) => {
 }
 
 let frame;
+//Repeating function to update the display
 let updateDisplay = () => {
+  //Wait time between frames for opencv
   let wait = 10;
 
+  //Display the webcam feed
   if(displayMode == 0){
     frame = camera.read();
   }
 
+  //Display the video for score reveal
   if(displayMode == 1){
+    //Decrease wait time between frames
     wait = 5;
     if(scores.red > scores.blue){
       frame = redVideo.read();
@@ -69,12 +86,16 @@ let updateDisplay = () => {
     }else { //Tie Game
       frame = tieVideo.read();
     }
+    //If video finished
     if(frame.empty){
+      //Reset the videos to starting points
       redVideo.reset();
       blueVideo.reset();
       tieVideo.reset();
+      //Display scores
       displayMode = 2;
       setTimeout(() => {
+        //After 7 seconds, change to display feed again and reset scores
         displayMode = 0;
         scores.blue = 0;
         scores.red = 0;
@@ -83,6 +104,7 @@ let updateDisplay = () => {
   }
 
   if (displayMode == 2) {
+    //Display scores and overlay score text
     const mat = cv.imread('./media/score-bg2.png');
     mat.putText(
       ""+scores.blue,
@@ -107,17 +129,22 @@ let updateDisplay = () => {
     )
     frame = mat;
   }
+  //If we have something to display
   if(!frame.empty){
-  if(displayMode == 1){
-    frame = frame.resize(768, Math.floor(frame.cols/frame.rows*768), cv.INTER_AREA)
-  }else {
-    frame = frame.resize(Math.floor(frame.rows/frame.cols*1024), 1024, cv.INTER_AREA)
+    //Resizing to fit the projector aspect ratio and resolution
+    if(displayMode == 1){
+      //Resize based on height (video feed is too wide)
+      frame = frame.resize(768, Math.floor(frame.cols/frame.rows*768), cv.INTER_AREA)
+    }else {
+      //Resize based on width (camera and image are too tall)
+      frame = frame.resize(Math.floor(frame.rows/frame.cols*1024), 1024, cv.INTER_AREA)
+    }
+
+    //Display the image in the window
+    cv.imshow('view', frame);
+    cv.waitKey(1);
   }
-
-
-  cv.imshow('view', frame);
-  cv.waitKey(1);
-}
+  //Update feed at 30 FPS so we have time to process web requests
   setTimeout(updateDisplay, 1000/30)
 };
 
